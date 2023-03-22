157775460d900a9a1d583171d099c4a3e2c9e309
Andreas Marius Laursen <amlaursen1998@gmail.com>
2023-03-22 21:04:56 +0100

    problem 1 and 2

---
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -0,0 +1,184 @@
+
+from types import SimpleNamespace
+
+import numpy as np
+from scipy import optimize
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+class HouseholdSpecializationModelClass:
+
+    def __init__(self):
+        """ setup model """
+
+        # a. create namespaces
+        par = self.par = SimpleNamespace()
+        sol = self.sol = SimpleNamespace()
+
+        # b. preferences
+        par.rho = 2.0
+        par.nu = 0.001
+        par.epsilon = 1.0
+        par.omega = 0.5 
+
+        # c. household production
+        par.alpha = 0.5
+        par.sigma = 1.0
+
+        # d. wages
+        par.wM = 1.0
+        par.wF = 1.0
+        par.wF_vec = np.linspace(0.8,1.2,5)
+
+        # e. targets
+        par.beta0_target = 0.4
+        par.beta1_target = -0.1
+
+        # f. solution
+        sol.LM_vec = np.zeros(par.wF_vec.size)
+        sol.HM_vec = np.zeros(par.wF_vec.size)
+        sol.LF_vec = np.zeros(par.wF_vec.size)
+        sol.HF_vec = np.zeros(par.wF_vec.size)
+
+        sol.beta0 = np.nan
+        sol.beta1 = np.nan
+
+    def calc_utility(self,LM,HM,LF,HF):
+        """ calculate utility """
+
+        par = self.par
+        sol = self.sol
+
+        # a. consumption of market goods
+        C = par.wM*LM + par.wF*LF
+
+        # b. home production
+        if par.sigma == 0.0:
+            H = np.min(HM,HF)
+
+        elif par.sigma == 1.0:
+            H = HM**(1-par.alpha)*HF**par.alpha
+
+        else:
+            exponent = (par.sigma-1)/par.sigma
+
+            # special case for negative exponent as we cannot divide by 0
+            if exponent < 0.0:
+                
+                # initialize terms of utility
+                term1 = np.full(HM.size,np.inf)
+                term2 = np.full(HM.size,np.inf)
+                H = np.zeros(HM.size)
+
+                # only for non-zero indices of HM and HF
+                zero_indices_HM = (HM == 0) 
+                zero_indices_HF = (HF == 0)
+                term1[~zero_indices_HM] = (1 - par.alpha) * HM[~zero_indices_HM]**exponent
+                term2[~zero_indices_HF] = par.alpha * HF[~zero_indices_HF]**exponent
+                
+                # only for non-infinity indices of term1 and term2
+                inf_indices = (term1 == np.inf) | (term2 == np.inf)
+                H[~inf_indices] = (term1[~inf_indices] + term2[~inf_indices])**(1/exponent)
+            
+            else:
+                term1 = (1 - par.alpha) * HM**exponent
+                term2 = par.alpha * HF**exponent
+                H = (term1 + term2)**(1/exponent)
+
+        # c. total consumption utility
+        Q = C**par.omega*H**(1-par.omega)
+        utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
+
+        # d. disutlity of work
+        epsilon_ = 1+1/par.epsilon
+        TM = LM+HM
+        TF = LF+HF
+        disutility = par.nu*(TM**epsilon_/epsilon_+TF**epsilon_/epsilon_)
+        
+        return utility - disutility
+
+    def solve_discrete(self,do_print=False):
+        """ solve model discretely """
+        
+        par = self.par
+        sol = self.sol
+        opt = SimpleNamespace()
+        
+        # a. all possible choices
+        x = np.linspace(0,24,49)
+        LM,HM,LF,HF = np.meshgrid(x,x,x,x) # all combinations
+    
+        LM = LM.ravel() # vector
+        HM = HM.ravel()
+        LF = LF.ravel()
+        HF = HF.ravel()
+
+        # b. calculate utility
+        u = self.calc_utility(LM,HM,LF,HF)
+    
+        # c. set to minus infinity if constraint is broken
+        I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
+        u[I] = -np.inf
+    
+        # d. find maximizing argument
+        j = np.argmax(u)
+        
+        opt.LM = LM[j]
+        opt.HM = HM[j]
+        opt.LF = LF[j]
+        opt.HF = HF[j]
+
+        # e. print
+        if do_print:
+            for k,v in opt.__dict__.items():
+                print(f'{k} = {v:6.4f}')
+
+        return opt
+
+    def solve(self,do_print=False):
+        """ solve model continously """
+
+        par = self.par
+        sol = self.sol
+
+        LM = np.nan # vector
+        HM = np.nan
+        LF = np.nan
+        HF = np.nan
+
+        varlist = [LM, HM, LF, HF]
+
+        # a. contraint function (negative if violated)
+        constraints = ({'type': 'ineq', 'fun': lambda LM,HM: LM + HM - 24},
+                       {'type': 'ineq', 'fun': lambda LF,HF: LF + HF - 24})
+        bounds = [(0,24) for x in varlist]
+
+        # b. call optimizer
+        initial_guess = 6 # some guess, should be feasible
+        res = optimize.minimize(
+            lambda x: self.calc_utility(LM, HM, LF, HF), initial_guess,
+            method='SLSQP', bounds=bounds, constraints=constraints)
+
+        print(res.message) # check that the solver has terminated correctly
+
+    def solve_wF_vec(self,discrete=False):
+        """ solve model for vector of female wages """
+
+        pass
+
+    def run_regression(self):
+        """ run regression """
+
+        par = self.par
+        sol = self.sol
+
+        x = np.log(par.wF_vec)
+        y = np.log(sol.HF_vec/sol.HM_vec)
+        A = np.vstack([np.ones(x.size),x]).T
+        sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
+    
+    def estimate(self,alpha=None,sigma=None):
+        """ estimate alpha and sigma """
+
+        pass
