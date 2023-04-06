d99332cb3ca26b3bdb98634472b8ba5d240d1840
Andreas Marius Laursen <amlaursen1998@gmail.com>
2023-04-07 00:29:24 +0200

    data project - almost done

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Predefine options for plots
+plt.rcParams.update({"axes.grid":True,"grid.color":"black","grid.alpha":"0.25","grid.linestyle":"-"})
+plt.rcParams.update({'font.size': 10})
+
 def clean_taxes():
     'clean the SKAT data'
 
@@ -39,10 +43,6 @@ def clean_wages():
 def plot_wages_taxes(merged):
     'plot wages and taxes from data'
 
-    # Predefine options for plots
-    plt.rcParams.update({"axes.grid":True,"grid.color":"black","grid.alpha":"0.25","grid.linestyle":"-"})
-    plt.rcParams.update({'font.size': 10})
-
     # Create a subplot with three plots
     fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(17.5, 5))
 
@@ -55,8 +55,8 @@ def plot_wages_taxes(merged):
     # Plot wage sum
     merged.plot(x='year', y='wage_sum', legend=False, ax=ax2)
     ax2.set_xlabel('Year')
-    ax2.set_ylabel('Billion DKK')
     ax2.set_title('Wage Sum')
+    ax2.set_ylabel('Billion DKK')
 
     # Plot first differences
     merged.plot(x='year', y='wage_sum_change', ax=ax3)
@@ -66,4 +66,26 @@ def plot_wages_taxes(merged):
     ax3.set_title('First differences')
 
     # Display the plot
+    plt.show()
+
+def plot_predicted_taxes(merged, reg=False):
+    'plot personal income taxes predicted from mean implicit tax rate'
+
+    # Create plot
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+    # Plot actual revenues and predicted revenues
+    merged.plot(x='year', y='personal_income_taxes', ax=ax1)
+    merged.plot(x='year', y='predicted_taxes', ax=ax1)
+    ax1.set_xlabel('Year')
+    ax1.set_ylabel('Billion DKK')
+    ax1.set_title('Predictions from implicit tax rate')
+
+    # Plot actual revenues and predicted revenues
+    merged.plot(x='year', y='personal_income_taxes', ax=ax2)
+    merged.plot(x='year', y='predicted_taxes_reg', ax=ax2)
+    ax2.set_xlabel('Year')
+    ax2.set_ylabel('Billion DKK')
+    ax2.set_title('Predictions from linear regression')
+
     plt.show()
